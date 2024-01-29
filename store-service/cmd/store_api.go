2f2224bc204d8ca82538683039f334226fae76dc
Luís Santana <luis.santana@ua.pt>
2024-01-29 17:07:15 +0000

    adds docker

--- store-service/cmd/store_api.go
+++ store-service/cmd/store_api.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	router := gin.Default()
-	//router.GET("/items", getItems)
-	//router.POST("/addItem", addItem)
-	//router.DELETE("/removeItem", removeItem)
 
-	router.Run("localhost:8081")
 }
