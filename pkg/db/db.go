f77188713f70429fe633e9017cd5a2bf139164b6
Luís Santana <luis.santana@ua.pt>
2024-01-30 14:28:25 +0000

    added connection to database

---
+++ pkg/db/db.go
@@ -0,0 +1,53 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/luissantanaa/online-store/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDb() {
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+
+	// dsn := os.Getenv("DSN")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to connecto to database: %s\n", err)
+		os.Exit(2)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Client{})
+	db.AutoMigrate(&models.Order{})
+	db.AutoMigrate(&models.Item{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+
+}
